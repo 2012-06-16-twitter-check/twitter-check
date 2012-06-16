45c53d0b8435930ef11ae675df160afb4321b449
Andrej A Antonov <polymorphm@gmail.com>
2012-06-16 19:40:46 +0400

    using urllib2 and implemented proxy

--- lib_twitter_check_2012_06_16/twitter_check.py
+++ lib_twitter_check_2012_06_16/twitter_check.py
@@ -21,9 +21,10 @@ assert str is bytes
 import itertools, functools, datetime
 import tornado.ioloop, tornado.stack_context, tornado.gen, \
         tornado.httpclient, tornado.escape
+from .async_http_request_helper import async_fetch
 
 DEFAULT_CONCURRENCE = 10
-DEFAULT_DELAY = 20.0
+DEFAULT_DELAY = 60.0
 
 def get_username(account_line):
     account = account_line.split(':')
@@ -33,34 +34,43 @@ def get_username(account_line):
     return account[0]
 
 @tornado.gen.engine
-def check_account(acc_meta, delay=None, callback=None):
+def check_account(acc_meta, delay=None, proxies=None, callback=None):
     callback = tornado.stack_context.wrap(callback)
     
     if delay is None:
         delay = DEFAULT_DELAY
+    delay_deadline = datetime.timedelta(seconds=delay)
     
-    wait_key = object()
-    http_client = tornado.httpclient.AsyncHTTPClient()
-    http_client.fetch(acc_meta['url'],
-            follow_redirects=False,
-            callback=(yield tornado.gen.Callback(wait_key)))
+    io_loop = tornado.ioloop.IOLoop.instance()
     
-    response = yield tornado.gen.Wait(wait_key)
+    def on_error(type, value, traceback):
+        if isinstance(value, EnvironmentError):
+            io_loop.add_timeout(
+                    delay_deadline,
+                    functools.partial(callback, False))
+            
+            return True
     
-    if not response.error and response.code == 200 and \
-            acc_meta['username'] in response.body:
+    with tornado.stack_context.ExceptionStackContext(on_error):
+        wait_key = object()
+        async_fetch(acc_meta['url'], proxies=proxies,
+                callback=(yield tornado.gen.Callback(wait_key)))
+        
+        response = yield tornado.gen.Wait(wait_key)
+    
+    if response.code == 200 and acc_meta['username'] in response.body:
         result = True
     else:
         result = False
     
     if callback is not None:
-        deadline = datetime.timedelta(seconds=delay)
-        tornado.ioloop.IOLoop.instance().add_timeout(
-                deadline,
+        io_loop.add_timeout(
+                delay_deadline,
                 functools.partial(callback, result))
 
 @tornado.gen.engine
-def check_list(in_list, conc=None, delay=None, on_positive=None, on_finish=None):
+def check_list(in_list, conc=None, delay=None, proxies=None,
+        on_positive=None, on_finish=None):
     on_finish = tornado.stack_context.wrap(on_finish)
     on_positive = tornado.stack_context.wrap(on_positive)
     
@@ -77,7 +87,7 @@ def check_list(in_list, conc=None, delay=None, on_positive=None, on_finish=None)
                 {
                     'account_line': account_line,
                     'username': get_username(account_line),
-                    'url': 'https://mobile.twitter.com/{username}'.format(
+                    'url': 'http://mobile.twitter.com/{username}'.format(
                             username=tornado.escape.url_escape(get_username(account_line)))
                 } for account_line in conc_in_list)
         
@@ -86,7 +96,7 @@ def check_list(in_list, conc=None, delay=None, on_positive=None, on_finish=None)
                     acc_meta['username'], acc_meta['url']))
             
             acc_meta['wait_key'] = object()
-            check_account(acc_meta, delay=delay,
+            check_account(acc_meta, delay=delay, proxies=proxies,
                     callback=(yield tornado.gen.Callback(acc_meta['wait_key'])))
         
         for acc_meta in acc_meta_list:
@@ -107,7 +117,7 @@ def check_list(in_list, conc=None, delay=None, on_positive=None, on_finish=None)
         on_finish()
 
 @tornado.gen.engine
-def check_list_files(in_list_files, conc=None, delay=None,
+def check_list_files(in_list_files, conc=None, delay=None, proxies=None,
         out_list=None, callback=None, on_positive=None):
     callback = tornado.stack_context.wrap(callback)
     on_positive = tornado.stack_context.wrap(on_positive)
@@ -138,7 +148,7 @@ def check_list_files(in_list_files, conc=None, delay=None,
                 on_positive(account_line, username)
     
     wait_key = object()
-    check_list(in_list, conc=conc, delay=delay,
+    check_list(in_list, conc=conc, delay=delay, proxies=proxies,
             on_finish=(yield tornado.gen.Callback(wait_key)),
             on_positive=on_check_list_files_positive)
     yield tornado.gen.Wait(wait_key)
